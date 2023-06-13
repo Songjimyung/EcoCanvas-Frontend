d7c2921fb1da0b026105b81823538ee70d25e314
Soeun_Jang <azulcielo_@naver.com>
2023-06-13 14:40:16 +0900

    <Fix> kakaocallback 함수 access,refresh,payload 로컬스토리지에 저장

--- src/pages/kakaocallback.js
+++ src/pages/kakaocallback.js
@@ -10,12 +10,20 @@ const CallbackKakao = (props) => {
       try {
         const response = await axios.get(`http://localhost:8000/users/oauth/kakao/callback/?code=${code}`);
         const token_req_json = response.data;
+        localStorage.setItem('access', token_req_json['jwt_token']['access']);
+        localStorage.setItem("refresh", token_req_json['jwt_token']['refresh']);
+        const base64Url = token_req_json['jwt_token']['access'].split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+        localStorage.setItem("payload", jsonPayload);
         let error = token_req_json.error;
         if (error !== null) {
           throw new Error(error);
         }
-        let access_token = token_req_json.access_token;
-        console.log(access_token);
+
+        console.log('카카오로그인 성공')
       } catch (error) {
         console.error(error);
       }
@@ -26,7 +34,7 @@ const CallbackKakao = (props) => {
 
   return (
     <div>
-      dddddddddddddfsdafasdfasdfasddfsd
+      카카오 로그인 성공
     </div>
   );
 };
