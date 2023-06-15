95060cdbc0af8ddc43f91ff2b3661cd823e44702
Soeun_Jang <azulcielo_@naver.com>
2023-06-15 14:38:52 +0900

    <Fix> 주문 내역 모달창 안불러와지는 현상 수정 #22

--- src/components/mypageSidebar/mypageSidebar.jsx
+++ src/components/mypageSidebar/mypageSidebar.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./mypageSidebar.css";
 import { Link, useLocation } from "react-router-dom";
 import SidebarItem from "../SidebarItem/SidebarItem";
 
 function Sidebar() {
+  const [userData, setUserData] = useState([])
+  const [userId, setUserId] = useState();
+  const [selectedInfo, setSelectedInfo] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   // URL의 path값을 받아올 수 있다.
   const pathName = useLocation().pathname;
 
+  //마이페이지 리스트
   const menus = [
     { name: "캠페인 참가 내역", path: "/mypage" },
     { name: "캠페인 작성 내역", path: "/mypage/mypostcampaign" },
@@ -20,13 +26,93 @@ function Sidebar() {
     { name: "환불/취소 접수", path: "/mypage/myrefund" }
   ];
 
+  // payload에서 user_id 가져와서 GET요청 
+  useEffect(() => {
+    const token = localStorage.getItem('access');
+    const payload = localStorage.getItem('payload');
+    if (payload) {
+      const payloadObject = JSON.parse(payload);
+      console.log(payloadObject.user_id)
+      setUserId(payloadObject.user_id);
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem('access');
+        const response = await fetch(`http://127.0.0.1:8000/users/${userId}/`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (response.ok) {
+          const result = await response.json();
+          console.log(result);
+          const user_info = result.map((user) => ({
+            username: user.username,
+            email: user.email,
+            id: user.id
+          }));
+          setUserData(user_info);
+        } else {
+          console.log('Failed to fetch user data');
+        }
+      } catch (error) {
+        console.log('Error while fetching user data:', error);
+      }
+    };
+
+    if (userId) {
+      fetchUserData();
+    }
+  }, [userId]);
+
+  //모달 open, close
+  const openModal = (userId) => {
+    setSelectedInfo(userId);
+    const modal = document.getElementById("modal");
+    modal.style.display = "block";
+  };
+  const closeModal = () => {
+    setSelectedInfo(null);
+    const modal = document.getElementById("modal");
+    modal.style.display = "none";
+  };
+
+  //회원정보 수정 
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+
+
   return (
     <>
       <div className="mypage-sidebar-wrap1">
         <div className="mypage-wrap">
           <div className="myprofile">
           </div>
-          <p>이름:</p>
+          <div>
+            {userData.map((user) => {
+              return (
+                <div key={user.id}>
+                  이름:{user.username}
+                  <p>이메일:{user.email}</p>
+                  <button className="details-button" onClick={() => openModal(user.id)}>회원정보 수정</button>
+                </div>
+              );
+            })}
+          </div>
           <div className="mypage-sidebar">
             <h3>나의 캠페인</h3>
             {menus.map((menu, index) => {
@@ -55,7 +141,37 @@ function Sidebar() {
           </div>
         </div>
       </div>
+      <div id="modal" className="modal">
+        <div className="modal-content">
+          <span className="close-button" onClick={closeModal}>&times;</span>
+          {selectedInfo && (
+            <div>
+              {userData.map((user) => {
+                if (user.id === selectedInfo) {
+                  return (
+                    <form onSubmit={handleSubmit} key={user.id}>
+                      <label>
+                        이메일:
+                        <input type="email" value={email} onChange={handleEmailChange} />
+                      </label>
+                      <br />
+                      <label>
+                        비밀번호:
+                        <input type="password" value={password} onChange={handlePasswordChange} />
+                      </label>
+                      <br />
+                      <button type="submit">저장</button>
+                      <button type="submit">회원탈퇴</button>
+                    </form>
+                  );
+                }
+                return null;
+              })}
+            </div>
+          )}
 
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/mypagelist/myOrder.jsx
+++ src/pages/mypagelist/myOrder.jsx
@@ -1,14 +1,134 @@
-import React from "react";
-import Sidebar from "../../components/mypageSidebar/MypageSidebar"
-import "../../components/mypageSidebar/mypageSidebar.css"
-
+import React, { useEffect, useState } from "react";
+import Sidebar from "../../components/mypageSidebar/MypageSidebar";
+import "../../components/mypageSidebar/mypageSidebar.css";
+import "../../css/mypage.css";
+import { format } from 'date-fns';
+import Pagination from '@mui/material/Pagination';
 
 const MyOrder = () => {
+  const [myorderData, setMyOrderData] = useState([]);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [currentOrderPage, setCurrentOrderPage] = useState(1);
+  const cardsPerPage = 5;
+
+  useEffect(() => {
+    const token = localStorage.getItem('access');
+
+    fetch("http://127.0.0.1:8000/shop/mypage/order/", {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+      .then(response => response.json())
+      .then(result => {
+        const myorders = result.map((order) => ({
+          id: order.id,
+          product: order.product,
+          order_totalprice: order.order_totalprice,
+          status: order["order_info"][0]["status"],
+          zip_code: order.zip_code,
+          address: order.address,
+          address_detail: order.address_detail,
+          address_message: order.address_message,
+          order_date: format(new Date(order.order_date), 'yyyy-MM-dd'),
+          order_quantity: order.order_quantity,
+          receiver_name: order.receiver_name,
+          receiver_number: order.receiver_number
+        }));
+        setMyOrderData(myorders);
+      });
+  }, []);
+
+  const handleOrderPageChange = (event, value) => {
+    setCurrentOrderPage(value);
+  };
+
+  const indexOfLastOrderCard = currentOrderPage * cardsPerPage;
+  const indexOfFirstOrderCard = indexOfLastOrderCard - cardsPerPage;
+  const currentOrderCards = myorderData.slice(indexOfFirstOrderCard, indexOfLastOrderCard);
+
+  const openModal = (orderId) => {
+    setSelectedOrder(orderId);
+    const modal = document.getElementById("orderModal");
+    modal.style.display = "block";
+  };
+
+  const closeModal = () => {
+    setSelectedOrder(null);
+    const modal = document.getElementById("orderModal");
+    modal.style.display = "none";
+  };
+
+  const selectedOrderData = myorderData.find(order => order.id === selectedOrder);
+
   return (
-    <div className="mypage-container">
+    <div className="mypage-block">
       <Sidebar />
-      <div>
-        주문내역 페이지
+      <div className="order-table-container">
+        <h1>주문 내역</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>주문 번호</th>
+              <th>상품명</th>
+              <th>가격</th>
+              <th>상태</th>
+              <th>세부 정보</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentOrderCards.map((order) => (
+              <tr key={order.id}>
+                <td>{order.id}</td>
+                <td>{order.product}</td>
+                <td>{order.order_totalprice}</td>
+                <td>{order.status}</td>
+                <td><button className="details-button" onClick={() => openModal(order.id)}>세부 정보 보기</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="order-pagination">
+          <Pagination
+            count={Math.ceil(myorderData.length / cardsPerPage)}
+            page={currentOrderPage}
+            onChange={handleOrderPageChange}
+            color="primary"
+            size="large"
+          />
+        </div>
+      </div>
+      <div id="orderModal" className="modal">
+        <div className="modal-content">
+          <span className="close-button" onClick={closeModal}>&times;</span>
+          {selectedOrderData && (
+            <div>
+              <div className="order-details">
+                <h2>주문 정보</h2>
+                <div>
+                  <p>주문 번호: {selectedOrderData.id}</p>
+                  <p>수령인: {selectedOrderData.receiver_name}</p>
+                  <p>상품명: {selectedOrderData.product}</p>
+                  <p>가격: {selectedOrderData.order_totalprice}원</p>
+                  <p>주문상태: <span style={{ color: 'blue' }}>{selectedOrderData.status}</span></p>
+                  <p>주문일: {selectedOrderData.order_date}</p>
+                </div>
+              </div>
+              <div className="shipping-details">
+                <h2>배송 정보</h2>
+                <div>
+                  <p>주문수량: {selectedOrderData.order_quantity}</p>
+                  <p>우편번호: {selectedOrderData.zip_code}</p>
+                  <p>주소: {selectedOrderData.address}</p>
+                  <p>상세주소: {selectedOrderData.address_detail}</p>
+                  <p>배송메세지: {selectedOrderData.address_message}</p>
+                  <p>연락처: {selectedOrderData.receiver_number}</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
