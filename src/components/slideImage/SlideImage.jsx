9ed8c757397825f0affb1744f5d0bd3c220e654e
I Joo Han <juhanlee2218@gmail.com>
2023-06-22 02:14:00 +0900

    Enhancement : 메인페이지 화면 구성 메인페이지 화면 구성 Enhancement

--- src/components/slideImage/SlideImage.jsx
+++ src/components/slideImage/SlideImage.jsx
@@ -1,34 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import './slideImage.css';
-
-// function SlideImage() {
-//   const [marginLeft, setMarginLeft] = useState('0px');
-
-//   useEffect(() => {
-//     const slideInterval = setInterval(() => {
-//       setMarginLeft('-1200px');
-//       setTimeout(() => {
-//         setMarginLeft('0px');
-//         const slideul = document.querySelector('.slideul');
-//         slideul.appendChild(slideul.firstElementChild);
-//       }, 80);
-//     }, 10000);
-
-//     // 컴포넌트가 unmount될 때 interval 정리
-//     return () => clearInterval(slideInterval);
-//   }, []);
-
-//   return (
-//     <section className="slide">
-//       <ul className="slideul" style={{ marginLeft: marginLeft }}>
-//         <li className="slide1"></li>
-//         <li className="slide2"></li>
-//         <li className="slide3"></li>
-//       </ul>
-//     </section>
-//   );
-// }
-
 import React, { useEffect, useRef } from "react";
 import "./slideImage.css";
 
@@ -38,10 +7,10 @@ function SlideImage() {
 
   useEffect(() => {
     let currentIndex = 0;
-    const interval = 10000; // 이미지 전환 간격 (10초)
+    const interval = 10000;
 
     function fadeIn(element) {
-      let op = 0.1; // 초기 투명도 값
+      let op = 0.1;
       element.style.display = "block";
       const timer = setInterval(() => {
         if (op >= 1) {
@@ -54,7 +23,7 @@ function SlideImage() {
     }
 
     function fadeOut(element) {
-      let op = 1; // 초기 투명도 값
+      let op = 1;
       const timer = setInterval(() => {
         if (op <= 0.1) {
           clearInterval(timer);
@@ -74,13 +43,11 @@ function SlideImage() {
       fadeIn(nextImage);
     }
 
-    // 이미지 노드 참조 설정
     images.current = Array.from(imagesRef.current.getElementsByTagName("li"));
 
-    fadeIn(images.current[currentIndex]); // 첫 번째 이미지 보이기
-    const intervalId = setInterval(changeImage, interval); // 일정 시간마다 이미지 전환
+    fadeIn(images.current[currentIndex]);
+    const intervalId = setInterval(changeImage, interval);
 
-    // 컴포넌트 언마운트 시 setInterval 정리
     return () => clearInterval(intervalId);
   }, []);
 
