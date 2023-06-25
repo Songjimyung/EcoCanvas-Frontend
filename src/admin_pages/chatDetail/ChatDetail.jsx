da515dadb5c2bc16c39c3cf26fa456ad6e187df7
Soeun_Jang <azulcielo_@naver.com>
2023-06-25 16:36:25 +0900

    pull 받기 전 임시 커밋

--- src/admin_pages/chatDetail/ChatDetail.jsx
+++ src/admin_pages/chatDetail/ChatDetail.jsx
@@ -6,101 +6,101 @@ import { useLocation } from 'react-router-dom';
 import Messages from "./messages/messages";
 
 export default function ChatDetail() {
-    const [messages, setMessages] = useState([])
-    const chatMessageInputRef = useRef(null);
-    const chatMessageSendRef = useRef(null);
-    let chatSocket = useRef(null);
-    let payload = localStorage.getItem('payload');
-    payload = JSON.parse(payload)
-    const userId = payload.user_id
-    const email = payload.email
-    const token = localStorage.getItem('access')
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const id = searchParams.get('id');
-    const [roomId, setRoomId] = useState('');
+  const [messages, setMessages] = useState([])
+  const chatMessageInputRef = useRef(null);
+  const chatMessageSendRef = useRef(null);
+  let chatSocket = useRef(null);
+  let payload = localStorage.getItem('payload');
+  payload = JSON.parse(payload)
+  const userId = payload.user_id
+  const email = payload.email
+  const token = localStorage.getItem('access')
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const id = searchParams.get('id');
+  const [roomId, setRoomId] = useState('');
 
-    useEffect(() => {
-        async function getRoomId() {
-            if (id) {
-                setRoomId(id)
-            }
-            else {
-                const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/chat/room/`, {
-                    headers: {
-                        "Authorization": `Bearer ${token}`,
-                    },
-                })
-                setRoomId(response.data.id)
-            }
+  useEffect(() => {
+    async function getRoomId() {
+      if (id) {
+        setRoomId(id)
+      }
+      else {
+        const response = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/chat/room/`, {
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
+        })
+        setRoomId(response.data.id)
+      }
+    }
+    getRoomId()
+
+    chatMessageInputRef.current.focus();
+
+    chatMessageInputRef.current.onkeyup = function (e) {
+      if (e.keyCode === 13) {
+        chatMessageSendRef.current.click();
+      }
+    };
+
+    chatMessageSendRef.current.onclick = function () {
+      if (chatMessageInputRef.current.value.length === 0) return;
+      chatSocket.current.send(JSON.stringify({
+        "message": chatMessageInputRef.current.value,
+        'user_id': userId,
+        'command': 'new_message'
+      }));
+      chatMessageInputRef.current.value = "";
+    };
+
+  }, [id, token, userId]);
+
+  useEffect(() => {
+    function connect() {
+      chatSocket.current = new WebSocket(`${process.env.REACT_APP_WEBSOCK_URL}/chat/${roomId}/?token=${token}`);
+
+      chatSocket.current.onopen = function (e) {
+        console.log("연결됨")
+        chatSocket.current.send(JSON.stringify({
+          'command': 'fetch_messages',
+          'user_id': userId,
+        }));
+      };
+
+      chatSocket.current.onclose = function (e) {
+        console.log("Close Socket")
+      };
+      chatSocket.current.onmessage = function (e) {
+        const data = JSON.parse(e.data);
+
+        if (data['command'] === 'messages') {
+          for (let i = 0; i < data['messages'].length; i++) {
+            setMessages((messages) => [...messages, data['messages'][i]])
+          }
+        } else if (data['command'] === 'new_message') {
+          setMessages((messages) => [...messages, data['message']])
         }
-        getRoomId()
+      };
 
-        chatMessageInputRef.current.focus();
+      chatSocket.current.onerror = function (err) {
+        chatSocket.current.onclose();
+      };
+    }
+    connect();
+    return () => {
+      chatSocket.current.close();
+    };
+  }, [roomId, token, userId]);
 
-        chatMessageInputRef.current.onkeyup = function (e) {
-            if (e.keyCode === 13) {
-                chatMessageSendRef.current.click();
-            }
-        };
-
-        chatMessageSendRef.current.onclick = function () {
-            if (chatMessageInputRef.current.value.length === 0) return;
-            chatSocket.current.send(JSON.stringify({
-                "message": chatMessageInputRef.current.value,
-                'user_id': userId,
-                'command': 'new_message'
-            }));
-            chatMessageInputRef.current.value = "";
-        };
-
-    }, [id, token, userId]);
-
-    useEffect(() => {
-        function connect() {
-            chatSocket.current = new WebSocket(`${process.env.REACT_APP_WEBSOCK_URL}/chat/${roomId}/?token=${token}`);
-
-            chatSocket.current.onopen = function (e) {
-                
-                chatSocket.current.send(JSON.stringify({
-                    'command': 'fetch_messages',
-                    'user_id': userId,
-                }));
-            };
-                
-            chatSocket.current.onclose = function (e) {
-                console.log("Close Socket")
-            };
-            chatSocket.current.onmessage = function (e) {
-                const data = JSON.parse(e.data);
-                
-                if (data['command'] === 'messages') {
-                    for (let i = 0; i < data['messages'].length; i++) {
-                        setMessages((messages) => [...messages, data['messages'][i]])
-                    }
-                } else if (data['command'] === 'new_message') {
-                    setMessages((messages) => [...messages, data['message']])
-                }
-            };
-
-            chatSocket.current.onerror = function (err) {
-                chatSocket.current.onclose();
-            };
-        }
-        connect();
-        return () => {
-            chatSocket.current.close();
-        };
-    }, [roomId, token, userId]);
-
-    return (
-        <div className="chatContainer">
-            <div className='container'>
-                <Messages messages={messages} name={email} />
-                <input className="input" type="text" id="chatMessageInput" ref={chatMessageInputRef} placeholder="전송하려는 메시지를 입력하세요."/>
-                <button className="sendButton" id="chatMessageSend" ref={chatMessageSendRef}>Send</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="chatContainer">
+      <div className='container'>
+        <Messages messages={messages} name={email} />
+        <input className="input" type="text" id="chatMessageInput" ref={chatMessageInputRef} placeholder="전송하려는 메시지를 입력하세요." />
+        <button className="sendButton" id="chatMessageSend" ref={chatMessageSendRef}>Send</button>
+      </div>
+    </div>
+  );
 }
 export { ChatDetail };
--- src/admin_pages/chatList/ChatList.jsx
+++ src/admin_pages/chatList/ChatList.jsx
@@ -4,65 +4,66 @@ import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function ChatList() {
-    const [chatList, setChatList] = useState([]);
-    const [userList, setUserList] = useState([]);
+  const [chatList, setChatList] = useState([]);
+  const [userList, setUserList] = useState([]);
 
-    useEffect(() => {
-        const getChatList = async () => {
-            const token = localStorage.getItem('access');
-            const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/chat/info/`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            const result = await response.json();
-            setChatList(result);
-        };
+  useEffect(() => {
+    const getChatList = async () => {
+      const token = localStorage.getItem('access');
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/chat/info/`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const result = await response.json();
+      console.log(result)
+      setChatList(result);
+    };
 
-        getChatList();
-    }, []);
+    getChatList();
+  }, []);
 
-    useEffect(() => {
-        const getUserData = async () => {
-            const userIds = chatList.map(chatItem => chatItem.advisee);
-            const token = localStorage.getItem('access');
-            const fetchUserPromises = userIds.map(async userId => {
-                try {
-                    const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users/${userId}/`, {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            'Authorization': `Bearer ${token}`
-                        },
-                    });
-                    const result = await response.json();
-                    return result;
-                } catch (error) {
-                    console.error(error);
-                    return null;
-                }
-            });
-            const users = await Promise.all(fetchUserPromises);
-            setUserList(users);
-        };
+  useEffect(() => {
+    const getUserData = async () => {
+      const userIds = chatList.map(chatItem => chatItem.advisee);
+      const token = localStorage.getItem('access');
+      const fetchUserPromises = userIds.map(async userId => {
+        try {
+          const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users/${userId}/`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+          });
+          const result = await response.json();
+          return result;
+        } catch (error) {
+          console.error(error);
+          return null;
+        }
+      });
+      const users = await Promise.all(fetchUserPromises);
+      setUserList(users);
+    };
 
-        getUserData();
-    }, [chatList]);
-    return (
+    getUserData();
+  }, [chatList]);
+  return (
     <div>
-        <h1>Chat List</h1>
-        {userList.map((user, index) => (
-            <div key={index}>
-                <h2>{user.email}</h2>
-                <Link to={`/chat?id=${chatList[index].id}`}>
-                <Button style={{ fontSize: '20px', color: 'red', margin: 'auto' }}>채팅</Button>
-                </Link>
-            </div>
-        ))}
+      <h1>Chat List</h1>
+      {userList.map((user, index) => (
+        <div key={index}>
+          <h2>{user.email}</h2>
+          <Link to={`/chat?id=${chatList[index].id}`}>
+            <Button style={{ fontSize: '20px', color: 'red', margin: 'auto' }}>채팅</Button>
+          </Link>
+        </div>
+      ))}
     </div>
-    )
+  )
 }
 
 export { ChatList };
